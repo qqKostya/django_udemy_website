b2777ee431536edbd852105a325f48a64cffa791
qqKostya <leap74rus@gmail.com>
2022-05-03 07:34:34 +0300

    add app crm and first models

--- landingpagewebsite/landingpagewebsite/settings.py
+++ landingpagewebsite/landingpagewebsite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crm.apps.CrmConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'landingpagewebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
